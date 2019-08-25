2c69193be33b7c3b4cdf37bf2f12dac88d218314
saguiguilid <tbki.jea@gmail.com>
2019-08-25 14:55:16 +0800

    Build CRUD App in .NET Core using CQRS + Mediator Pattern
    * Implement CRUD
    * Implement Thin API Controllers
    * Seed more data
    * Add more migrations
    * MediatR
    * The CQRS + MediatR Pattern

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
-using Domain;
 using Persistence;
+using MediatR;
+using Application.Activities;
 
 namespace API
 {
@@ -37,6 +31,7 @@ namespace API
                 service.AddPolicy("CorsPolicy", policy =>
                 policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000"));
             });
+            services.AddMediatR(typeof(List.Query).Assembly);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
---
+++ Application/Activities/Detail.cs
@@ -0,0 +1,34 @@
+using MediatR;
+using Domain;
+using System.Threading;
+using System.Threading.Tasks;
+using Persistence;
+using System;
+
+namespace Application.Activities
+{
+    public class Detail
+    {
+        public class Query : IRequest<Activity>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Activity>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                return activity;
+            }
+        }
+    }
+}
